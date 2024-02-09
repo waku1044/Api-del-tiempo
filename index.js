a6332d2919e9c0a10088c904135e20257081897c
Walter Gimenez <75543829+waku1044@users.noreply.github.com>
2024-02-09 20:26:17 -0300

    prueba 3

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const crearLineaNueva = (ciudad,temp,hume,presion,viento,espera,min,max)=>{
         document.querySelector('#max').innerHTML = max        
     }
     
-// 'lluvia ligera','nubes','nubes dispersas','cielo claro','lluvia de gran intensidad','lluvia moderada','muy nuboso'
+// 'lluvia ligera','nubes','nubes dispersas','cielo claro','lluvia de gran intensidad','lluvia moderada','muy nuboso','tormenta con lluvia'
 // https://i2.wp.com/medioambienteynaturaleza.com/wp-content/uploads/2015/08/Nubes-ar.jpg?ssl=1  2
 // http://static.tiempo.com.mx/uploads/imagen/imagen/388071/principal_lluvia.jpg  1
 // https://image.freepik.com/foto-gratis/nubes-dispersas-cielo_5219-2193.jpg  3
@@ -28,6 +28,7 @@ const crearLineaNueva = (ciudad,temp,hume,presion,viento,espera,min,max)=>{
 // https://www.blogdelfotografo.com/wp-content/uploads/2018/12/rain-275317_1920.jpg  5
 // https://e.rpp-noticias.io/xlarge/2023/04/08/103010_1412656.jpg  6
 // http://www.blogodisea.com/wp-content/uploads/2010/06/lluvia-cielo-nublado-plomizo-waxing-storm-campo.jpg  7
+//https://services.meteored.com/img/article/alerta-fuertes-lluvias-y-tormentas-en-el-litoral-fin-de-semana-340521-1_1280.jpg  8
 const ciudad  = prompt('Cual es tu la ciudad?'); 
 const actualiza = ()=>{
     window.location.href = './index.html';
@@ -37,12 +38,12 @@ console.log(ciudad)
 
  async function api(ciudad){   
     try {
-         const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad === '' ? 'campana' : ciudad},AR&APPID=5af0f382c935c41627351f02286325a7&units=metric&lang=es`)
+         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad === '' ? 'campana' : ciudad},AR&APPID=5af0f382c935c41627351f02286325a7&units=metric&lang=es`)
          const data = await response.json();
          console.log(data.weather[0].description)
          switch (data.weather[0].description) {
              case 'lluvia ligera':
-                 img_tiempo.src = 'http://static.tiempo.com.mx/uploads/imagen/imagen/388071/principal_lluvia.jpg';
+                 img_tiempo.src = 'https://static.tiempo.com.mx/uploads/imagen/imagen/388071/principal_lluvia.jpg';
                  img_tiempo.setAttribute('alt', 'lluvia ligera');
                  break;
              case 'nubes':
@@ -66,9 +67,13 @@ console.log(ciudad)
                  img_tiempo.setAttribute('alt', 'lluvia moderada');
                  break;
              case 'muy nuboso':
-                 img_tiempo.src = 'http://www.blogodisea.com/wp-content/uploads/2010/06/lluvia-cielo-nublado-plomizo-waxing-storm-campo.jpg';
+                 img_tiempo.src = 'https://www.blogodisea.com/wp-content/uploads/2010/06/lluvia-cielo-nublado-plomizo-waxing-storm-campo.jpg';
                  img_tiempo.setAttribute('alt', 'muy nuboso');
                  break;
+             case 'tormenta con lluvia':
+                 img_tiempo.src = 'https://services.meteored.com/img/article/alerta-fuertes-lluvias-y-tormentas-en-el-litoral-fin-de-semana-340521-1_1280.jpg';
+                 img_tiempo.setAttribute('alt', 'tormenta con lluvia');
+                 break; 
          }
         crearLineaNueva(data.name, data.main.temp, data.main.humidity, data.main.pressure, data.wind.speed, data.weather[0].description, data.main.temp_min, data.main.temp_max);
      } catch (err) {
